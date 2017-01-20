0bc16803f671a5fff3e11a6368d68e05385d5bf2
Justin Vallely <justinvallely@gmail.com>
2017-01-19 17:01:10 -0700

    Initial Commit

---
+++ Custom Animation Tests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom Animation Tests
+//
+//  Created by Justin Vallely on 1/19/17.
+//  Copyright © 2017 Pajama Donkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
